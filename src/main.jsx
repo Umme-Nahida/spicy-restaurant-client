742403b8e113a3f96660d82d2918e26053f86d09
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-24 21:37:31 +0600

    setup react query

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,21 @@ import './index.css'
 import router from './Router/router.jsx';
 import AuthProvider from './Authentication/AuthProvider/AuthProvider.jsx';
 import { Toaster } from 'react-hot-toast';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
+    
     </AuthProvider>
     <Toaster/>
   </StrictMode>,
