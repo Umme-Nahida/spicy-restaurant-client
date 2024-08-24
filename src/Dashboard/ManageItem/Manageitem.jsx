a8f3b02f578d32c7bac4b37de1e7e0ca14026687
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-25 01:19:22 +0600

    added managaItem page

---
+++ src/Dashboard/ManageItem/Manageitem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Manageitem = () => {
+    return (
+        <div className='bg-red-600'>
+            <h1>this is manage item page</h1>
+        </div>
+    );
+};
+
+export default Manageitem;
